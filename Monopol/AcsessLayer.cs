0f193eb33b679156a9aed22b58929d790133324f
Erik Skagen Vindenes <eriksv88@gmail.com>
2021-10-24 20:53:45 +0200

    Added new acsessplayer

---
+++ Monopol/AcsessLayer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITMonopoly
+{
+
+    public class AcsessLayer
+    {
+
+        public static DataSet Exec(string ProcedureName, SqlParameterCollection sqlParameters)
+        {
+            var dataset = new DataSet();
+
+            using (var conn = new SqlConnection(UserSession.ConnectionString))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    da.SelectCommand = new SqlCommand(ProcedureName, conn);
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                    da.Fill(dataset);
+                }
+            }
+         
+            return dataset;
+        }
+        protected void SetupNotifier()
+        {
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(@"SELECT [FullName],[experiance_nYears] FROM [dbo].[t_Doctor]", connection))
+                {
+                    command.Notification = null;
+                    SqlDependency dependency = new SqlDependency(command);
+                    dependency.OnChange += new OnChangeEventHandler(dependency_OnChange);
+                    if (connection.State == ConnectionState.Closed)
+                    {
+                        connection.Open();
+                    }
+                    var reader = command.ExecuteReader();
+                    reader.Close();
+                }
+            }
+        }
+
+        private void dependency_OnChange(object sender, SqlNotificationEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ Monopol/UserSession.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITMonopoly
+{
+    public static class UserSession
+    {
+
+        public static string ConnectionString = "Server=DESKTOP-CKDEOPD;Database=Monopol;Trusted_Connection=True;";
+        public static void Init()
+        {
+            DataSet dataset = AcsessLayer.Exec("stp_Monopol_CreateSessionPlayer", null);
+            if (dataset.Tables.Count < 1) { return; }
+            if (dataset.Tables[0].Rows.Count < 1) { return; }
+            TempUserID = (int)dataset.Tables[0].Rows[0]["UserID"];
+            TempSessionID = (int)dataset.Tables[0].Rows[0]["SessionID"];
+        }
+
+        private static int TempUserID;
+        public static int UserID { 
+            get {
+                if(TempUserID==0)
+                {
+                    Init();
+                }
+
+                return TempUserID;
+            } 
+            set
+            {
+                TempUserID = value;
+            }
+        }
+
+        private static int TempSessionID;
+        public static int SessionID { 
+        get
+            {
+                if (TempSessionID == 0)
+                {
+                    Init();
+                }
+                return TempSessionID;
+            } 
+            set
+            {
+                TempSessionID = value;
+            }
+        }
+        
+    }
+}
